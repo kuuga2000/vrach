bf56f9fd4149757f63b9970c3768b731733ad4c9
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-08-03 17:21:26 +0700

    VRC-5 - delete unused code

--- app/src/main/java/com/example/vrach/ui/register/RegisterForm.kt
+++ app/src/main/java/com/example/vrach/ui/register/RegisterForm.kt
@@ -11,33 +11,28 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.vrach.R
@@ -220,23 +215,7 @@ fun StepBioRegisterForm(
     var email by remember { mutableStateOf("") }
     var gender by remember { mutableStateOf("0") }
 
-
-/*    var isContextMenuVisible by rememberSaveable {
-        mutableStateOf(false)
-    }
-    var pressOffset by remember {
-        mutableStateOf(DpOffset.Zero)
-    }
-    var itemHeight by remember {
-        mutableStateOf(0.dp)
-    }
-    val interactionSource = remember {
-        MutableInteractionSource()
-    }
-    val density = LocalDensity.current
-    val dropdownItems = listOf("Food", "Bill Payment", "Recharges", "Outing", "Other")*/
     val options = listOf("Male", "Female", "Unknown")
-
     var expanded by remember { mutableStateOf(false) }
     var selectedOptionText by remember { mutableStateOf("") }
 
@@ -268,24 +247,6 @@ fun StepBioRegisterForm(
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            /*DropdownMenu(
-                expanded = isContextMenuVisible,
-                onDismissRequest = {
-                    isContextMenuVisible = false
-                },
-                offset = pressOffset.copy(
-                    y = pressOffset.y - itemHeight
-                )
-            ) {
-                dropdownItems.forEach {selectionOption ->
-                    DropdownMenuItem(
-                        text = {Text(selectionOption)},
-                        onClick = {
-                            isContextMenuVisible = false
-                        }
-                    )
-                }
-            }*/
             OutlinedTextField(
                 value = fullname,
                 onValueChange = {fullname = it},
@@ -362,25 +323,6 @@ fun StepBioRegisterForm(
                 ),
                 modifier = modifier
             )
-            /*OutlinedTextField(
-                value = gender,
-                onValueChange = {gender = it},
-                label = {
-                    Text("Gender")
-                },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Ascii,
-                    imeAction = ImeAction.Done
-                ),
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    focusedBorderColor = Color(0xFFF246BFD),
-                    unfocusedBorderColor = PurpleWhite,
-                    focusedLabelColor = Color(0xFFF246BFD),
-                    cursorColor = Color(0xFFF246BFD),
-                    containerColor = genderColor
-                ),
-                modifier = modifier
-            )*/
             ExposedDropdownMenuBox(
                 expanded = expanded,
                 onExpandedChange = {
